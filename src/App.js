7fdc476466cd30fc3c15e5acaa6339c0759b3cd1
balaji1610 <balajiece1610@gmail.com>
2023-04-06 21:45:44 +0530

    install_bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Balaji</h1>
+      <div class="btn-group" role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-primary">
+          Left
+        </button>
+        <button type="button" class="btn btn-primary">
+          Middle
+        </button>
+        <button type="button" class="btn btn-primary">
+          Right
+        </button>
+      </div>
     </div>
   );
 }
